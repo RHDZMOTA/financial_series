20804f1822985500a8e46f13071e17b45d063d36
Rodrigo Hernández Mota <rohdzmota@gmail.com>
2016-11-11 13:58:12 -0600

    add aux_fun.py script into the main script

--- forex_data.py
+++ forex_data.py
@@ -15,9 +15,52 @@ import matplotlib.pyplot as plt
 #import pandas.io.data as wb
 #import pandas_datareader as pdr
 #from pandas_datareader.oanda import get_oanda_currency_historical_rates as get_oanda
-from aux_fun import *
+#from aux_fun import *
 import datetime as dt
 import os.path
+
+# %% Aux. Functions
+# Function to calculate returns: 
+def calc_rtns(prices, warning = True, met_log = True):
+    
+    # Warning function 
+    def warn(sti = 0):
+        w0 = 'Error: datatype must be numpy.ndarray'
+        if sti == 0:
+            sti = w0
+        print(sti)
+        return None 
+    
+    # identify variable type for prices
+    ty = type(prices)
+    if warning:
+        # DataFrame 
+        if ty == type(pd.DataFrame([])):
+            if np.shape(prices)[1] == 1:
+                prices = prices.values
+            else:
+                sti = 'Error: variable is a DataFrame, must be numpy.ndarray'
+                return warn(sti)
+        # Series 
+        elif ty == type(pd.Series([])):
+            prices = prices.values 
+        # None 
+        elif ty == type(None):
+            sti = 'Error: The varialbe does not contain data (None)'
+            return warn(sti)
+        # Anything different to numpy.ndarray
+        elif ty != type(np.array([])):
+            return warn()
+    
+    # Calculate the returns 
+    if met_log:
+        returns = np.log(prices[1:] / prices[0:-1])
+    else:
+        returns = prices[1:] / prices[0:-1] - 1 
+    
+    return returns         
+
+# %% Class
 
 class currency:
     '''
